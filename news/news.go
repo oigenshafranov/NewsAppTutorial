ce562744be3be3b68a9fe188af0f549149ffa074
oigenshafranov <oigenshafranov@gmail.com>
2021-07-12 23:38:13 +0300

    FifthCommit

--- news/news.go
+++ news/news.go
@@ -1,11 +1,34 @@
 package news
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type Client struct {
 	http     *http.Client
 	key      string
 	PageSize int
+}
+
+type Article struct {
+	Source struct {
+		ID   interface{} `json:"id"`
+		Name string      `json:"name"`
+	} `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
 }
 
 func NewClient(httpClient *http.Client, key string, pageSize int) *Client {
