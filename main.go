d5d9923b0369b2707fbf91e548fb6d1e2f808830
oigenshafranov <oigenshafranov@gmail.com>
2021-07-13 00:19:43 +0300

    SixthCommit

--- main.go
+++ main.go
@@ -34,8 +34,15 @@ func searchHandler(newsapi *news.Client) http.HandlerFunc {
 			page = "1"
 		}
 
-		fmt.Println("Search Query is:", searchQuery)
-		fmt.Print("Page is:", page)
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println("%+v", results)
+		// fmt.Println("Search Query is:", searchQuery)
+		// fmt.Print("Page is:", page)
 
 	}
 }
